5a160a54151309875c080f0a8524a0a7a0089fe7
Praveen Gupta <66120416+pkgtech@users.noreply.github.com>
2020-09-28 08:12:58 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
 function menu(){
-    $(".line-nav").toggle();
+   $("#navbar").toggleClass("hide");
 }
-
-function fun() {
-    // $(".aa").css("display", "block");
-    $(".aa").show("slow");
-    $("form").css("margin-top", "30%");
-}
-
-$(document).ready(function(){
-    $('body').scrollspy({ target: '#navbar' })
-
-    $('#sm-navbar a').click(function(){ $(".line-nav").toggle(); })
-    
-});
